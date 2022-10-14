2b1491980b413a7451a20f6c76d15d22204d4e9a
Korodee <koroskki@gmail.com>
2022-10-14 15:39:48 +0100

    Setting up React app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Components } from "react";
+import "./App.css";
+
+class App extends Components {
+  render() {
+    return <div className="App">
+      <h1>Krode</h1>
+    </div>;
+  }
+}
+
+export default App;
